ab4853fb21dde669e60fcb5f7c4059fc5fdac650
steve <stevetasticsteve@gmail.com>
2021-06-11 14:04:13 +0100

    Added intended first apps

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask
 from flask import render_template
+from flask import url_for
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
+def index():
     return render_template('index.html')
+
+@app.route("/phonemics")
+def phonemics():
+    return render_template('phonemics.html')
+
+@app.route("/verb-prediction")
+def verb_prediction():
+    return render_template('verb_prediction.html')
 
 
 if __name__ == "__main__":
