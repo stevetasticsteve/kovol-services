e17bbd5d9adf4c2ec4e89b5faaf09742bd209e20
steve <stevetasticsteve@gmail.com>
2021-09-20 17:38:39 +0100

    Heroku tweak

--- app.py
+++ app.py
@@ -87,4 +87,5 @@ class VerbPredictionForm(FlaskForm):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=3000)
+    # app.run(debug=True, host="0.0.0.0", port=3000)
+    app.run()
